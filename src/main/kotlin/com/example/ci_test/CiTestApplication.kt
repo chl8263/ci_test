8635385605bb663974c594a8c73287b7d8f47853
Ewan <ewan@Ewans-MacBook-Pro.local>
2021-08-29 17:01:08 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/ci_test/CiTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ci_test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CiTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<CiTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/ci_test/CiTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ci_test
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CiTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
